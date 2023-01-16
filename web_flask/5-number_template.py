c447aea25ffa61423a43282c77aa6f640801952a
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-01-16 15:36:53 +0300

    Task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+'''Starts a Flask web application'''
+
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''Returns “Hello HBNB!”'''
+
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    '''Returns "HBNB"'''
+
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_var(text):
+    '''Returns C plus variable text'''
+
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/(<text>)', strict_slashes=False)
+def python_route(text="is cool"):
+    '''Displays “Python ”, followed by the value of the text variable'''
+
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    '''Display “n is a number” only if n is an integer'''
+
+    if (type(n) == int):
+        return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_temp(n):
+    '''Displays a HTML page only if n is an integer'''
+
+    if type(n) == int:
+        return render_template('5-number_template.py', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
